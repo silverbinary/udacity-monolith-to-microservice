cbc560264cc6f301550b1a15fc602fbd924c6b22
black-hoodie <tungvoson98@gmail.com>
2022-11-26 10:25:43 +0700

    project 3 upload

---
+++ udagram-api-user/src/config/config.ts
@@ -0,0 +1,27 @@
+export const config = {
+  // 'username': process.env.POSTGRES_USERNAME,
+  // 'password': process.env.POSTGRES_PASSWORD,
+  // 'database': process.env.POSTGRES_DB,
+  // 'host': process.env.POSTGRES_HOST,
+  // 'dialect': 'postgres',
+  // 'aws_region': process.env.AWS_REGION,
+  // 'aws_profile': process.env.AWS_PROFILE,
+  // 'aws_media_bucket': process.env.AWS_BUCKET,
+  // 'url': process.env.URL,
+  // 'jwt': {
+  //   'secret': process.env.JWT_SECRET,
+  // },
+  'username': "tungvs6db",
+  'password': "tungvs6db",
+  'database': "tungvs6db",
+  'host': "tungvs6db.cqj89w5qozqp.us-east-1.rds.amazonaws.com",
+  'dialect': "postgres",
+  'aws_region': "us-east-1",
+  'aws_profile': "udacity",
+  'aws_media_bucket': "tungvs6-pj3-bucket",
+  'url': "http://localhost:8100",
+  'jwt': {
+    'secret': "hello",
+  },
+};
+
