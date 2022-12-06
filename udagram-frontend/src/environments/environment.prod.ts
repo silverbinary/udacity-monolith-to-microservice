43bf5cdb69bcc0b5be065cbffd04ff9a006cf485
black-hoodie <tungvoson98@gmail.com>
2022-12-06 23:11:38 +0700

    update deployment file

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a9bffdcccb667490292e96cec166999e-281382932.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
